9589bde75b0518f4ae6310f3f092572589d4a0a1
Meghan Prestemon <m.m.hares@gmail.com>
2017-08-19 19:34:53 -0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+import Header from './components/Header.js';
+// import MessageList from './components/MessageList.js';
+// import Compose from './components/Compose.js';
+
+const App = () => (
+  <div>
+    <div className="ui text container">
+      <Header />
+      < />
+      < />
+    </div>
+  </div>
+)
+
+export default App;
